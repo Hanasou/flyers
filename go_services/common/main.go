e958117d04553140299a6cdd23680ed3dd3f49a2
Hanasou <royzhang42@gmail.com>
2022-11-19 19:41:07 -0800

    memdb skeleton

---
+++ go_services/common/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Hanasou/flyers/go_services/common/memdb"
+)
+
+func main() {
+	fmt.Println(memdb.NewKv().Store)
+}
---
+++ go_services/common/memdb/memdb.go
@@ -0,0 +1,55 @@
+package memdb
+
+import (
+	"math/rand"
+	"strconv"
+)
+
+// This will be an in-memory key value store that holds some data
+// This will mostly be for testing purposes, to test APIs before we connect to a remote database service
+
+type Entry struct {
+	data any
+}
+
+type Db struct {
+	Store map[string]Entry
+}
+
+func NewKv() *Db {
+	newStore := &Db{
+		map[string]Entry{},
+	}
+
+	populateStore(newStore)
+
+	return newStore
+}
+
+func UpsertElement(db *Db, key string, data any) {
+	db.Store[key] = Entry{
+		data,
+	}
+}
+
+func populateStore(db *Db) {
+	names := []string{
+		"Bill", "Bob", "Jack", "Jill",
+	}
+	eyeColors := []string{
+		"Black", "Blue", "Brown", "Green",
+	}
+	type person struct {
+		id       int
+		name     string
+		eyeColor string
+	}
+	for i := 0; i < 10; i++ {
+		newPerson := person{
+			id:       i,
+			name:     names[rand.Intn(3)],
+			eyeColor: eyeColors[rand.Intn(3)],
+		}
+		UpsertElement(db, strconv.Itoa(i), newPerson)
+	}
+}

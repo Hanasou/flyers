de8ed3023d96348056d311ab57abf1fbf0196732
Hanasou <royzhang42@gmail.com>
2023-04-21 20:54:08 -0700

    Removed common library dependency

--- go_services/gateway/go.mod
+++ go_services/gateway/go.mod
@@ -2,4 +2,3 @@ module github.com/Hanasou/flyers/go_services/gateway
 
 go 1.19
 
-require github.com/Hanasou/flyers/go_services/common v0.0.0-20221120011156-40ea6b64a311
--- go_services/gateway/main.go
+++ go_services/gateway/main.go
@@ -2,16 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Hanasou/flyers/go_services/common/shutdown"
-	"github.com/Hanasou/flyers/go_services/common/utils"
 )
 
 func main() {
 	fmt.Println("Hello from Gateway")
 
-	utils.HelloFromUtils()
-	utils.HelloFromUtils2()
-
-	shutdown.WaitForInterrupt()
 }

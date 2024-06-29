77b1e23253dec3fa0d0d7927f4689bb35ab96524
Hanasou <royzhang42@gmail.com>
2024-06-29 13:27:17 -0700

    I think it's just starting the server

--- go_services/gateway/main.go
+++ go_services/gateway/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Hanasou/flyers/go_services/common/graphql"
 )
 
 func main() {
 	fmt.Println("Hello from Gateway")
-
+	graphql.StartGraphqlServer()
 }
